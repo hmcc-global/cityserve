55f527be1c9c62035a32cd5d58ac19142179721d
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2023-11-14 13:46:46 +0800

    Added image & icon

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,7 +5,15 @@ const MainContainer = () => {
   return (
     <chakra.main minH="100%" minW="100%" flex={1} id="main-container">
       <Image
-        src={process.env.PUBLIC_URL + "images/city-serve.jpeg"}
+        src={process.env.PUBLIC_URL + "images/cityserve_laptop.png"}
+        display={["none", "block"]}
+        objectFit="fill"
+        w="100%"
+        h="100%"
+      />
+      <Image
+        src={process.env.PUBLIC_URL + "images/cityserve_mobile.png"}
+        display={["block", "image"]}
         objectFit="fill"
         w="100%"
         h="100%"
