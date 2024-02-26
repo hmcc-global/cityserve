a2b66126693448965f233bb29773927cc1693081
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2024-02-26 12:48:11 +0800

    changed to sign up link

--- src/components/JoinUsSection.js
+++ src/components/JoinUsSection.js
@@ -19,18 +19,18 @@ const JoinUsSection = () => {
         textColor="#183B5D"
         fontFamily="Gotham"
         fontWeight="900"
-        w={{ base: "60%", sm: "50%" }}
-        h={{ base: "4em" }}
+        w={{ base: "34%", sm: "35%" }}
+        h={{ base: "3em", sm: "2.4em" }}
         mt={{ base: "2" }}
         px={{ base: "4em", sm: "1em" }}
         textAlign="center"
         borderRadius={50}
         as={Link}
         target="_blank"
-        fontSize={{ base: "2vw", sm: "1.5vw" }}
-        href="https://docs.google.com/forms/u/0/d/1JduNnwWHdwP_bFVqDRZpmNSCqBkPg8iQU90w0F4JMPA/viewform?pli=1&pli=1&edit_requested=true"
+        fontSize={{ base: "2.3vw", sm: "2vw" }}
+        href="https://bit.ly/CityServe2024"
       >
-        FILL OUT THIS FORM IF YOU ARE <br /> INTERESTED AND WANT TO KNOW MORE!{" "}
+        SIGN UP NOW!{" "}
       </Button>
     </VStack>
   );
