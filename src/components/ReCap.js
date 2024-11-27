6df6cb220bbb0298670eedecef2a5c1dc83a9e82
cornyachtsorry <andrea.justkidding@gmail.com>
2024-11-28 02:09:11 +0800

    GH-5: Add mobile view

--- src/components/ReCap.js
+++ src/components/ReCap.js
@@ -1,4 +1,4 @@
-import { Text, VStack, AspectRatio, HStack, SimpleGrid, Image, Box} from "@chakra-ui/react";
+import { Text, VStack, AspectRatio, Stack, HStack, SimpleGrid, Image, Box, Center} from "@chakra-ui/react";
 
 const cardsData = [
   {
@@ -29,14 +29,17 @@ const ReCap = () => {
     <VStack w="100%" 
       bgColor="#F9EEE3" 
       gap ="5" 
-      p="5rem"
+      p={{sm: "5rem"}}
+      py={{base:"2.5rem"}}
+      px={{base:"1.313rem"}}
       textColor="#183B5D"
+
     >
       <Text
         fontFamily="Antonio"
         fontWeight="700"
-        fontSize={{ base: "3.5vw", sm: "2.25rem" }}
-        textAlign="center"
+        fontSize={{ base: "1.75rem", lg: "2.25rem" }}
+        textAlign="left"
       >
         A RECAP OF CITYSERVE 2024
       </Text>
@@ -44,62 +47,76 @@ const ReCap = () => {
       <Text
         fontFamily="Manrope"
         fontWeight="400"
-        fontSize={{ base: "3.5vw", sm: "1.25rem" }}
-        textAlign="center"
-        marginX={{base:"3.5rem", xl:"9rem"}}
+        fontSize={{ base: "0.875rem", lg: "1.25rem" }}
+        textAlign={{ base: "left", md:"center", lg: "center" }}
+        marginX={{ lg:"15rem"}}
       >
         The 2024 CityServe took place on Sunday, March 17th. Churches, organizations, and participants came together to 
         serve the different needs of the city. Check out the video to learn more about what happened and how we saw God’s 
         love demonstrated throughout the city! 
       </Text>
 
-      <HStack w="100%">
-        <AspectRatio width="42rem" ratio={16 / 9} marginLeft="7rem">
-          <iframe
-            title="video-embed"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            src="https://www.youtube.com/embed/wj7FnRB-0zk?si=5_1uy1aXIN_WIhMH"
-          />
-        </AspectRatio>
-
-        <SimpleGrid 
-          columns={{ base: 2, md: 2 }} 
-          spacing="1.25rem"
-          marginLeft="1rem"
-        >
-          {cardsData.map((card) => (
-            <Box
-              width="13.438rem"
-              height="11.25rem"
-              alignItems="center"
-              justifyContent="center"
-              backgroundImage={card.backgroundImage}
-            >
-              <VStack p="2rem"
+      <Stack 
+        direction={['column', 'row']} 
+        w="100%" 
+        marginTop={{ base: "1.313rem", sm:"0rem", lg: "2.5rem" }} 
+      >
+        <Center>
+          <AspectRatio 
+            width={{ base: "24rem", lg: "42rem" }}
+            ratio={16 / 9} 
+            marginLeft={{lg: "13rem"}}
+          >
+            <iframe
+              title="video-embed"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              src="https://www.youtube.com/embed/wj7FnRB-0zk?si=5_1uy1aXIN_WIhMH"
+            />
+          </AspectRatio>
+        </Center>
+       
+        <Center>
+          <SimpleGrid 
+            columns={{ base: 2, lg: 2 }} 
+            spacing={{ base: "0.938rem", lg: "1.25rem" }}
+            marginLeft={{sm: "1rem" }}
+            marginTop={{ base: "1.621rem", lg: "0rem" }}
+          >
+            {cardsData.map((card) => (
+              <Box
+                width={{ base: "9.813rem",  md:"6rem", lg: "13.438rem" }}
+                height={{ base: "8.188rem",  md:"7.125rem", lg: "11.25rem" }}
+                alignItems="center"
+                justifyContent="center"
+                backgroundImage={card.backgroundImage}
+                backgroundSize="contain"
               >
-                <Text 
-                  fontFamily="Antonio"
-                  fontSize={{ base: "1.75rem", md: "2.75rem" }}
-                  fontWeight="700"
-                  textAlign="center"
+                <VStack p={{ base: "1rem", lg: "2rem" }}
                 >
-                  {card.number}
-                </Text>
-                <Text 
-                  fontFamily="Manrope"
-                  fontSize={{ base: "1.75rem", md: "1rem" }}
-                  fontWeight="800"
-                  textAlign="center"
-                  letterSpacing="0.12rem"
-                >
-                  {card.title}
-                </Text>
-              </VStack>
-            </Box>
-          ))}  
-        </SimpleGrid>
-      </HStack>
+                  <Text 
+                    fontFamily="Antonio"
+                    fontSize={{ base: "2rem", sm:"2rem", lg: "2.75rem" }}
+                    fontWeight="700"
+                    textAlign="center"
+                  >
+                    {card.number}
+                  </Text>
+                  <Text 
+                    fontFamily="Manrope"
+                    fontSize={{ base: "0.875rem", sm:"0.5rem", lg: "1rem" }}
+                    fontWeight="800"
+                    textAlign="center"
+                    letterSpacing="0.12rem"
+                  >
+                    {card.title}
+                  </Text>
+                </VStack>
+              </Box>
+            ))}  
+          </SimpleGrid>
+        </Center>
+      </Stack>
     </VStack>
   );
 };
