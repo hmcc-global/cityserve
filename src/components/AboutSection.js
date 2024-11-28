262d852e042dfe3440acf1b665248ef6cab757d1
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2024-11-29 00:31:32 +0800

    GH-4: finished mobile version

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,24 +7,32 @@ import {
   HStack,
   Link,
   Img,
+  Button,
 } from "@chakra-ui/react";
 
 const AboutSection = () => {
   return (
     <>
-      <VStack w="100%" gap="0" fontFamily="Antonio" letterSpacing="0.05rem">
+      <VStack w="100%" gap="0" bgColor="#183B5D">
         <Box
           backgroundImage={{
             base: `url(${
-              process.env.PUBLIC_URL + "/images/cityserve_bg_base.jpg"
+              process.env.PUBLIC_URL + "/images/cityserve_bg_mobile.png"
             })`,
             md: `url(${process.env.PUBLIC_URL + "/images/cityserve_bg.jpg"})`,
           }}
-          backgroundPosition="center"
+          backgroundPosition={{ base: "bottom", md: "center" }}
           backgroundSize="cover"
           minW="100%"
         >
-          <HStack w="100%" h="90vh">
+          {/* Desktop version */}
+          <HStack
+            w="100%"
+            h="95vh"
+            display={{ base: "none", md: "flex" }}
+            letterSpacing="0.05em"
+            fontFamily="Antonio"
+          >
             <Flex justifyContent="center" w="50%">
               <Image
                 w="100%"
@@ -36,7 +44,7 @@ const AboutSection = () => {
               w="50%"
               paddingLeft="5vw"
               paddingY="auto"
-              fontWeight="800"
+              fontWeight="700"
               textColor="#183B5D"
             >
               <Text fontSize="1.8rem" marginBottom="5">
@@ -65,26 +73,102 @@ const AboutSection = () => {
               </Link>
             </Box>
           </HStack>
+
+          {/* Mobile version */}
+          <VStack
+            minH="170vw"
+            letterSpacing="0.01rem"
+            display={{ base: "flex", md: "none" }}
+            fontFamily="Antonio"
+          >
+            <Flex justifyContent="center" h="50%">
+              <Image
+                w="90%"
+                h="auto"
+                src={process.env.PUBLIC_URL + "images/cityserve_logo.png"}
+              />
+            </Flex>
+            <VStack
+              h="50%"
+              alignItems="start"
+              fontWeight="700"
+              textColor="#183B5D"
+              gap="4"
+            >
+              <Text fontSize="1.75rem" marginBottom="3">
+                APRIL 4-6, 2025
+              </Text>
+              <Text fontSize="2rem">
+                TO BE A{" "}
+                <span style={{ color: "#1769B8" }}>VISIBLE DISPLAY</span> AND{" "}
+                <br />
+                <span style={{ color: "#1769B8" }}>
+                  {" "}
+                  VIABLE DEMONSTRATION OF
+                </span>{" "}
+                <br />
+                GOD'S LOVE FOR THE CITY
+              </Text>
+              <Button
+                bgColor="#1769B8"
+                textColor="#F8F8F8"
+                fontSize="0.8rem"
+                fontWeight="800"
+                href="https://forms.gle/UoAwhzPYqgkUsdcf6"
+                as={Link}
+                target="_blank"
+                fontFamily="Manrope"
+                w="70%"
+                borderRadius="0.6rem"
+              >
+                2025 INTEREST FORM
+              </Button>
+              <Link display="flex" alignItems="center">
+                <Img
+                  src={process.env.PUBLIC_URL + "images/trianglevec_blue.svg"}
+                  boxSize={4}
+                  mr={2}
+                />
+                <Text textColor="#1769B8" letterSpacing="0.2rem">
+                  LEARN HOW YOU CAN TAKE PART
+                </Text>
+              </Link>
+            </VStack>
+          </VStack>
         </Box>
+
         <VStack
-          bgColor="#183B5D"
           pt="2"
           pb={{ base: "5", sm: "10" }}
           paddingTop="4vw"
+          fontFamily="Manrope"
         >
-          <HStack w="100%" alignItems="flex-start" paddingTop="5vw">
+          {/* Desktop version */}
+          <HStack
+            w="100%"
+            alignItems="flex-start"
+            paddingTop="5vw"
+            display={{ base: "none", md: "flex" }}
+            letterSpacing="0.05rem"
+          >
             <Flex w="50%" h="100%">
-              <Text textColor="#99C6E6" paddingLeft="10vw" fontSize="2.25rem">
+              <Text
+                textColor="#99C6E6"
+                paddingLeft="10vw"
+                fontSize="2.25rem"
+                fontFamily="Antonio"
+                fontWeight="700"
+              >
                 WHAT IS CITYSERVE?
               </Text>
             </Flex>
             <VStack
-              fontFamily="Gotham-light"
               fontSize="1.25rem"
               gap={{ base: "2", sm: "7" }}
               w="58%"
               textColor="#DAEDFA"
               paddingLeft="5vw"
+              paddingRight="9vw"
             >
               <Text>
                 CityServe is a city-wide movement to partner with local churches
@@ -98,8 +182,73 @@ const AboutSection = () => {
                 serving the poor, marginalised, and unreached people of Hong
                 Kong.
               </Text>
+              <Link w="100%" display="flex">
+                <Img
+                  src={process.env.PUBLIC_URL + "images/trianglevec_skin.svg"}
+                  boxSize={4}
+                  mr={2}
+                  my="auto"
+                />
+                <Text textColor="#E0CEBB" fontWeight="700">
+                  CHECK OUT WHAT HAPPENED IN CITYSERVE 2024
+                </Text>
+              </Link>
             </VStack>
           </HStack>
+
+          {/* Mobile version */}
+          <VStack
+            w="100%"
+            alignItems="flex-start"
+            paddingTop="3vw"
+            display={{ base: "flex", md: "none" }}
+            letterSpacing="0.01rem"
+          >
+            <Flex w="100%" h="100%" marginBottom="5">
+              <Text
+                textColor="#99C6E6"
+                paddingLeft="6vw"
+                fontSize="2.25rem"
+                fontFamily="Antonio"
+                fontWeight="700"
+              >
+                WHAT IS CITYSERVE?
+              </Text>
+            </Flex>
+            <VStack
+              fontSize="0.87rem"
+              gap="7"
+              w="100%"
+              textColor="#DAEDFA"
+              paddingLeft="6vw"
+              paddingRight="5vw"
+              fontFamily="Manrope"
+            >
+              <Text>
+                CityServe is a city-wide movement to partner with local churches
+                and organizations to serve others and demonstrate God’s love for
+                the city.
+              </Text>
+              <Text>
+                CityServe Hong Kong was initiated to see churches and
+                organizations coming together on a Sunday, to go beyond the four
+                walls of the church. We want to be God’s witness by loving and
+                serving the poor, marginalised, and unreached people of Hong
+                Kong.
+              </Text>
+              <Link w="100%" display="flex">
+                <Img
+                  src={process.env.PUBLIC_URL + "images/trianglevec_skin.svg"}
+                  boxSize={4}
+                  mr={2}
+                  my="auto"
+                />
+                <Text textColor="#E0CEBB" fontWeight="700">
+                  CHECK OUT WHAT HAPPENED <br /> IN CITYSERVE 2024
+                </Text>
+              </Link>
+            </VStack>
+          </VStack>
         </VStack>
       </VStack>
     </>
