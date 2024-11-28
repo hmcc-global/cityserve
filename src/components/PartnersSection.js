01c25e95b594f9a1450906f67ed4ce0fb58c8b04
Yoo Yuen Yau <yuenyauu@gmail.com>
2024-11-29 00:06:15 +0800

    GH-2: Added responsive design

--- src/components/PartnersSection.js
+++ src/components/PartnersSection.js
@@ -2,15 +2,12 @@ import {
 	Text,
 	VStack,
 	Image,
-	HStack,
-	Icon,
 	Container,
 	Box,
 	Flex,
+	Grid,
+	GridItem,
 } from '@chakra-ui/react';
-import { FaGlobe } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
-import { AiOutlineMail } from 'react-icons/ai';
 
 const PartnersSection = () => {
 	const PartnerDisplayBox = (imagePath, title) => {
@@ -24,8 +21,9 @@ const PartnersSection = () => {
 				borderRadius="10px"
 				borderColor="#F8F8F8"
 				boxShadow="0px 0px 3px 0px rgba(0, 0, 0, 0.2)"
-				p="1.5rem"
-				maxW="500px"
+				p="1.25rem"
+				maxW={{ base: '42.5vw', lg: '17.5vw', xl: '20vw' }}
+				h={'100%'}
 			>
 				<Image src={process.env.PUBLIC_URL + imagePath} h="6rem" mb={'1rem'} />
 				<Text
@@ -35,7 +33,7 @@ const PartnersSection = () => {
 					textTransform={'uppercase'}
 					textAlign={'center'}
 					wordBreak="break-word"
-					maxW="250px"
+					maxW={{ base: '37.5vw', lg: '12.5vw', xl: '15vw' }}
 				>
 					{title}
 				</Text>
@@ -45,20 +43,23 @@ const PartnersSection = () => {
 
 	return (
 		<Box maxW="100vw" bgColor="#FFFFFF" p={0}>
-			<Container maxW="container.xl" px={0} py="5rem">
-				<HStack
-					p={0}
+			<Container maxW="container.xl" py="4rem" px={0}>
+				<Box
+					py={0}
+					px={{ base: '1rem', lg: '0' }}
 					maxW="100%"
 					fontFamily="Gotham"
-					display={{ base: 'none', sm: 'flex' }}
+					display={'flex'}
+					flexDirection={{ base: 'column', lg: 'row' }}
 					alignItems="flex-start"
+					justifyContent="space-between"
+					gap={{ base: '2rem', lg: '2rem' }}
 				>
 					<VStack
 						textColor="#183B5D"
 						w="100%"
-						px="5"
 						gap="1rem"
-						width={{ base: '100vw', lg: '20vw' }}
+						width={{ base: '100%', lg: '20vw' }}
 						alignItems="flex-start"
 					>
 						<Text
@@ -78,6 +79,7 @@ const PartnersSection = () => {
 						</Text>
 					</VStack>
 					<Flex
+						display={{ base: 'none', lg: 'flex' }}
 						wrap={'wrap'}
 						justifyContent={'flex-end'}
 						textColor="#183B5D"
@@ -105,102 +107,47 @@ const PartnersSection = () => {
 							'jesus the living god international ministry'
 						)}
 					</Flex>
-				</HStack>
-				{/* Mobile version */}
-				<VStack display={{ base: 'flex', sm: 'none' }} pt="3">
-					<Text fontWeight={700} fontSize="4vw" mb="3">
-						PARTNERED WITH
-					</Text>
-					<HStack gap={0} paddingBottom={4}>
-						<VStack w="100%" pl="3">
-							<HStack justifyContent="center">
-								<Image
-									src={process.env.PUBLIC_URL + 'images/partnered_logo_2.png'}
-									w="19%"
-								/>
-								<Image
-									src={process.env.PUBLIC_URL + 'images/partnered_logo_3.png'}
-									w="25%"
-								/>
-								<Image
-									src={process.env.PUBLIC_URL + 'images/partnered_logo_4.png'}
-									w="27%"
-								/>
-								<Image
-									src={process.env.PUBLIC_URL + 'images/partnered_logo_5.png'}
-									w="17%"
-								/>
-							</HStack>
-							{/* <Text fontWeight={700} fontSize="4vw">
-              NGOs
-            </Text> */}
-						</VStack>
-						{/* <VStack w="60%">
-            <HStack justifyContent="center">
-              <Image
-                src={process.env.PUBLIC_URL + "images/partnered_logo_5.png"}
-                w="12%"
-              />
-              <Image
-                src={process.env.PUBLIC_URL + "images/partnered_logo_6.png"}
-                w="12%"
-              />
-              <Image
-                src={process.env.PUBLIC_URL + "images/partnered_logo_7.jpg"}
-                w="12%"
-              />
-              <Image
-                src={process.env.PUBLIC_URL + "images/partnered_logo_1.jpg"}
-                w="33%"
-              />
-            </HStack>
-            <Text fontWeight={700} fontSize="4vw">
-              Churches
-            </Text>
-          </VStack> */}
-					</HStack>
-					<VStack w="100%" textColor="#FFFFFF" bgColor="#183B5D" gap="0" pb="9">
-						<Text fontWeight={600} fontSize="5vw">
-							Contact Us
-						</Text>
-						<Text fontFamily="Gotham-light" fontWeight={100} fontSize="2vw">
-							if you are interested or if you have any questions!
-						</Text>
-						<HStack gap={1} pt="2">
-							<Icon
-								target="_blank"
-								bgColor="transparent"
-								fgColor="white"
-								as={FaInstagram}
-								boxSize={3}
-								url="https://www.instagram.com/cityservehk/"
-							/>
-							<Text fontSize="2vw">@cityservehk</Text>
-						</HStack>
-						<HStack gap={1}>
-							<Icon
-								target="_blank"
-								network="website"
-								as={FaGlobe}
-								boxSize={3}
-								url="https://www.cityservehk.org"
-							/>
-							<Text fontSize="2vw">cityservehk.org</Text>
-						</HStack>
-						<HStack gap={1}>
-							<Icon
-								target="_blank"
-								bgColor="transparent"
-								fgColor="white"
-								as={AiOutlineMail}
-								boxSize={3}
-								network="email"
-								url="mailto:cityserve@hongkong.hmcc.net"
-							/>
-							<Text fontSize="2vw">cityserve@hongkong.hmcc.net</Text>
-						</HStack>
-					</VStack>
-				</VStack>
+					<Box display={{ base: 'flex', lg: 'none' }} w="100%" p={0}>
+						<Grid templateColumns="repeat(2, 1fr)" gap={'4vw'}>
+							<GridItem>
+								{PartnerDisplayBox(
+									'images/partnered_logo_2.png',
+									'INNER CITY MINISTRIES'
+								)}
+							</GridItem>
+							<GridItem>
+								{PartnerDisplayBox(
+									'images/partnered_logo_3.png',
+									'hong kong church network for the poor'
+								)}
+							</GridItem>
+							<GridItem>
+								{PartnerDisplayBox(
+									'images/christian_action.png',
+									'christian action'
+								)}
+							</GridItem>
+							<GridItem>
+								{PartnerDisplayBox('images/manna.png', 'MANNA MINISTRY')}
+							</GridItem>
+							<GridItem>
+								{PartnerDisplayBox(
+									'images/mothers_choice.png',
+									"MOTHER'S CHOICE"
+								)}
+							</GridItem>
+							<GridItem>
+								{PartnerDisplayBox('images/s&d.png', 'SONS & DAUGHTER HK')}
+							</GridItem>
+							<GridItem>
+								{PartnerDisplayBox(
+									'images/partnered_logo_5.png',
+									'jesus the living god international ministry'
+								)}
+							</GridItem>
+						</Grid>
+					</Box>
+				</Box>
 			</Container>
 		</Box>
 	);
