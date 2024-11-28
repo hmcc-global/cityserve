021898baac3ef1f0f9bf01696aa81948cd6a5bc1
Yoo Yuen Yau <yuenyauu@gmail.com>
2024-11-28 23:31:36 +0800

    GH-3: Added section component

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,15 +1,16 @@
-import { Image, VStack, Flex } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 import AboutSection from "./AboutSection";
 import ReCap from "./ReCap";
 import JoinUsSection from "./JoinUsSection";
 import InfoSection from "./InfoSection";
-import NumbersSection from "./NumbersSection";
+import ParticipateSection from "./ParticipateSection";
 
 const HomeContainer = () => {
   return (
     <VStack gap={0}>
       <AboutSection />
       <ReCap />
+      <ParticipateSection />
       <JoinUsSection />
       <InfoSection />
     </VStack>
