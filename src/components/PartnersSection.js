2519c5d993080f1e9c69d678e819ae309525deca
Yoo Yuen Yau <yuenyauu@gmail.com>
2024-11-29 21:49:01 +0800

    GH-14: Add responsive font sizing

--- src/components/PartnersSection.js
+++ src/components/PartnersSection.js
@@ -28,7 +28,7 @@ const PartnersSection = () => {
 				<Image src={process.env.PUBLIC_URL + imagePath} h="6rem" mb={'1rem'} />
 				<Text
 					fontWeight={800}
-					fontSize="1rem"
+					fontSize={{ base: '0.875rem', md: '1rem' }}
 					fontFamily="'Manrope', sans-serif"
 					textTransform={'uppercase'}
 					textAlign={'center'}
@@ -42,113 +42,110 @@ const PartnersSection = () => {
 	};
 
 	return (
-			<Container maxW="container.xl" py="4rem" px={0}>
-				<Box
-					py={0}
-					px={{ base: '1rem', md: '2rem', lg: '3rem', xl: 0 }}
-					maxW="100%"
-					fontFamily="Gotham"
-					display={'flex'}
-					flexDirection={{ base: 'column', lg: 'row' }}
+		<Container maxW="container.xl" py="4rem" px={0}>
+			<Box
+				py={0}
+				px={{ base: '1rem', md: '2rem', lg: '3rem', xl: 0 }}
+				maxW="100%"
+				fontFamily="Gotham"
+				display={'flex'}
+				flexDirection={{ base: 'column', lg: 'row' }}
+				alignItems="flex-start"
+				justifyContent="space-between"
+				gap={{ base: '2rem', lg: '2rem' }}
+			>
+				<VStack
+					textColor="#183B5D"
+					w="100%"
+					gap="1rem"
+					width={{ base: '100%', lg: '20vw' }}
 					alignItems="flex-start"
-					justifyContent="space-between"
-					gap={{ base: '2rem', lg: '2rem' }}
 				>
-					<VStack
-						textColor="#183B5D"
-						w="100%"
-						gap="1rem"
-						width={{ base: '100%', lg: '20vw' }}
-						alignItems="flex-start"
+					<Text
+						fontWeight={800}
+						fontSize={{ base: '1.75rem', md: '2.25rem' }}
+						textTransform={'uppercase'}
+						fontFamily="'Antonio', sans-serif"
 					>
-						<Text
-							fontWeight={800}
-							fontSize="2.25rem"
-							textTransform={'uppercase'}
-							fontFamily="'Antonio', sans-serif"
-						>
-							Our Partners
-						</Text>
-						<Text
-							fontWeight={400}
-							fontSize="1.25rem"
-							fontFamily="'Manrope', sans-serif"
-						>
-							Organizations and churches we have partnership with. Click on each
-							card to learn more about them!
-						</Text>
-					</VStack>
-					<Flex
-						display={{ base: 'none', lg: 'flex' }}
-						wrap={'wrap'}
-						justifyContent={'flex-end'}
-						textColor="#183B5D"
-						flex={1}
-						gap={'1rem'}
-						maxW="100%"
+						Our Partners
+					</Text>
+					<Text
+						fontWeight={400}
+						fontSize={{ base: '0.875rem', md: '1.25rem' }}
+						fontFamily="'Manrope', sans-serif"
 					>
-						{PartnerDisplayBox(
-							'images/partnered_logo_2.png',
-							'INNER CITY MINISTRIES'
-						)}
-						{PartnerDisplayBox(
-							'images/partnered_logo_3.png',
-							'hong kong church network for the poor'
-						)}
-						{PartnerDisplayBox(
-							'images/christian_action.png',
-							'christian action'
-						)}
-						{PartnerDisplayBox('images/manna.png', 'MANNA MINISTRY')}
-						{PartnerDisplayBox('images/mothers_choice.png', "MOTHER'S CHOICE")}
-						{PartnerDisplayBox('images/s&d.png', 'SONS & DAUGHTER HK')}
-						{PartnerDisplayBox(
-							'images/partnered_logo_5.png',
-							'jesus the living god international ministry'
-						)}
-					</Flex>
-					<Box display={{ base: 'flex', lg: 'none' }} w="100%" p={0}>
-						<Grid templateColumns="repeat(2, 1fr)" gap={'6'}>
-							<GridItem>
-								{PartnerDisplayBox(
-									'images/partnered_logo_2.png',
-									'INNER CITY MINISTRIES'
-								)}
-							</GridItem>
-							<GridItem>
-								{PartnerDisplayBox(
-									'images/partnered_logo_3.png',
-									'hong kong church network for the poor'
-								)}
-							</GridItem>
-							<GridItem>
-								{PartnerDisplayBox(
-									'images/christian_action.png',
-									'christian action'
-								)}
-							</GridItem>
-							<GridItem>
-								{PartnerDisplayBox('images/manna.png', 'MANNA MINISTRY')}
-							</GridItem>
-							<GridItem>
-								{PartnerDisplayBox(
-									'images/mothers_choice.png',
-									"MOTHER'S CHOICE"
-								)}
-							</GridItem>
-							<GridItem>
-								{PartnerDisplayBox('images/s&d.png', 'SONS & DAUGHTER HK')}
-							</GridItem>
-							<GridItem>
-								{PartnerDisplayBox(
-									'images/partnered_logo_5.png',
-									'jesus the living god international ministry'
-								)}
-							</GridItem>
-						</Grid>
-					</Box>
+						Organizations and churches we have partnership with. Click on each
+						card to learn more about them!
+					</Text>
+				</VStack>
+				<Flex
+					display={{ base: 'none', lg: 'flex' }}
+					wrap={'wrap'}
+					justifyContent={'flex-end'}
+					textColor="#183B5D"
+					flex={1}
+					gap={'1rem'}
+					maxW="100%"
+				>
+					{PartnerDisplayBox(
+						'images/partnered_logo_2.png',
+						'INNER CITY MINISTRIES'
+					)}
+					{PartnerDisplayBox(
+						'images/partnered_logo_3.png',
+						'hong kong church network for the poor'
+					)}
+					{PartnerDisplayBox('images/christian_action.png', 'christian action')}
+					{PartnerDisplayBox('images/manna.png', 'MANNA MINISTRY')}
+					{PartnerDisplayBox('images/mothers_choice.png', "MOTHER'S CHOICE")}
+					{PartnerDisplayBox('images/s&d.png', 'SONS & DAUGHTER HK')}
+					{PartnerDisplayBox(
+						'images/partnered_logo_5.png',
+						'jesus the living god international ministry'
+					)}
+				</Flex>
+				<Box display={{ base: 'flex', lg: 'none' }} w="100%" p={0}>
+					<Grid templateColumns="repeat(2, 1fr)" gap={'6'}>
+						<GridItem>
+							{PartnerDisplayBox(
+								'images/partnered_logo_2.png',
+								'INNER CITY MINISTRIES'
+							)}
+						</GridItem>
+						<GridItem>
+							{PartnerDisplayBox(
+								'images/partnered_logo_3.png',
+								'hong kong church network for the poor'
+							)}
+						</GridItem>
+						<GridItem>
+							{PartnerDisplayBox(
+								'images/christian_action.png',
+								'christian action'
+							)}
+						</GridItem>
+						<GridItem>
+							{PartnerDisplayBox('images/manna.png', 'MANNA MINISTRY')}
+						</GridItem>
+						<GridItem>
+							{PartnerDisplayBox(
+								'images/mothers_choice.png',
+								"MOTHER'S CHOICE"
+							)}
+						</GridItem>
+						<GridItem>
+							{PartnerDisplayBox('images/s&d.png', 'SONS & DAUGHTER HK')}
+						</GridItem>
+						<GridItem>
+							{PartnerDisplayBox(
+								'images/partnered_logo_5.png',
+								'jesus the living god international ministry'
+							)}
+						</GridItem>
+					</Grid>
 				</Box>
-			</Container>
+			</Box>
+		</Container>
 	);
 };
 
