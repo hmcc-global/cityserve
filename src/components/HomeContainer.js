b0449116897c55d5245a6b337b3105ea1530f35d
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2024-12-01 01:08:30 +0800

    GH-17: finished with social needs

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -4,11 +4,14 @@ import ReCap from "./ReCap";
 import JoinUsSection from "./JoinUsSection";
 import ParticipateSection from "./ParticipateSection";
 import PartnersSection from "./PartnersSection";
+import SocialNeedSection from "./SocialNeedSection";
 
 const HomeContainer = ({ recapRef, participateRef }) => {
   return (
     <VStack gap={0}>
       <AboutSection recapRef={recapRef} />
+      <SocialNeedSection />
+      <JoinUsSection ref={participateRef} />
       <ReCap ref={recapRef} />
       <ParticipateSection />
       <JoinUsSection ref={participateRef} />
